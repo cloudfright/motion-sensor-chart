421cd43f54708959429899e71bde95e4e4518ae2
Ben Dyer <ben.dyer@hey.com>
2022-04-01 20:49:01 +0100

    Test smoothing

--- scripts/sensors.js
+++ scripts/sensors.js
@@ -12,6 +12,31 @@ var accellGravitySeries1 = new TimeSeries();
 var accellGravitySeries2 = new TimeSeries();
 var accellGravitySeries3 = new TimeSeries();
 
+class ExponentialMovingAverage {
+	constructor(alpha, mean) {
+		this.alpha = alpha;
+		this.mean = !mean ? 0 : mean;
+	}
+
+	get beta() {
+		return 1 - this.alpha;
+	}
+
+    get filtered() {
+		return this.mean;
+	}
+
+	update(newValue) {
+		const redistributedMean = this.beta * this.mean;
+		const meanIncrement = this.alpha * newValue;
+		const newMean = redistributedMean + meanIncrement;
+		this.mean = newMean;
+	}
+}
+
+let smoothAx;
+
+
 function createTimeline() {
 
     var gyroChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
@@ -25,6 +50,9 @@ function createTimeline() {
     accellChart.addTimeSeries(accellSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
     accellChart.addTimeSeries(accellSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
     accellChart.streamTo(document.getElementById("accel-ng-chart"), 500);
+
+    smoothAx = new ExponentialMovingAverage(0.7);
+
 
     var accellGravityChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
     accellGravityChart.addTimeSeries(accellGravitySeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
@@ -52,6 +80,8 @@ function requestPermission() {
   }
 }
 
+
+
 function handleOrientation(event) {
 
   var now = Date.now();
@@ -59,9 +89,14 @@ function handleOrientation(event) {
   gyroSeries2.append(now, event.rotationRate.beta);
   gyroSeries3.append(now, event.rotationRate.gamma);
 
+  smoothAx.update(event.acceleration.x);
+
   accellSeries1.append(now, event.acceleration.x);
-  accellSeries2.append(now, event.acceleration.y);
+  accellSeries2.append(now, smoothAx.filtered());
+  // accellSeries2.append(now, event.acceleration.y);
   accellSeries3.append(now, event.acceleration.z);
+
+  
 
   accellGravitySeries1.append(now, event.accelerationIncludingGravity.x - event.acceleration.x);
   accellGravitySeries2.append(now, event.accelerationIncludingGravity.y - event.acceleration.y);
