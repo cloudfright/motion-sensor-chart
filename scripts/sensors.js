326825d8076f0bd366e9403cb6b7f9fb598dae84
Ben Dyer <ben.dyer@hey.com>
2022-04-02 15:39:19 +0100

    Tidy up

--- scripts/sensors.js
+++ scripts/sensors.js
@@ -1,63 +1,36 @@
 
 
-var gyroSeries1 = new TimeSeries();
-var gyroSeries2 = new TimeSeries();
-var gyroSeries3 = new TimeSeries();
+var rotationRateSeries1 = new TimeSeries();
+var rotationRateSeries2 = new TimeSeries();
+var rotationRateSeries3 = new TimeSeries();
 
-var accellSeries1 = new TimeSeries();
-var accellSeries2 = new TimeSeries();
-var accellSeries3 = new TimeSeries();
+var accelerationSeries1 = new TimeSeries();
+var accelerationSeries2 = new TimeSeries();
+var accelerationSeries3 = new TimeSeries();
 
-var accellGravitySeries1 = new TimeSeries();
-var accellGravitySeries2 = new TimeSeries();
-var accellGravitySeries3 = new TimeSeries();
-
-class ExponentialMovingAverage {
-	constructor(alpha, mean) {
-		this.alpha = alpha;
-		this.mean = !mean ? 0 : mean;
-	}
-
-	get beta() {
-		return 1 - this.alpha;
-	}
-
-    get filtered() {
-		return this.mean;
-	}
-
-	update(newValue) {
-		const redistributedMean = this.beta * this.mean;
-		const meanIncrement = this.alpha * newValue;
-		const newMean = redistributedMean + meanIncrement;
-		this.mean = newMean;
-	}
-}
-
-let smoothAx;
-
+var orientationSeries1 = new TimeSeries();
+var orientationSeries2 = new TimeSeries();
+var orientationSeries3 = new TimeSeries();
 
 function createTimeline() {
 
-    var gyroChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
-    gyroChart.addTimeSeries(gyroSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
-    gyroChart.addTimeSeries(gyroSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
-    gyroChart.addTimeSeries(gyroSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
-    gyroChart.streamTo(document.getElementById("gryo-chart"), 500);
+    var rotationRateChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
+    rotationRateChart.addTimeSeries(rotationRateSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
+    rotationRateChart.addTimeSeries(rotationRateSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
+    rotationRateChart.addTimeSeries(rotationRateSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
+    rotationRateChart.streamTo(document.getElementById("rotation-rate-chart"), 500);
 
-    var accellChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
-    accellChart.addTimeSeries(accellSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
-    accellChart.addTimeSeries(accellSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
-    accellChart.addTimeSeries(accellSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
-    accellChart.streamTo(document.getElementById("accel-ng-chart"), 500);
+    var accelerationChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
+    accelerationChart.addTimeSeries(accelerationSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
+    accelerationChart.addTimeSeries(accelerationSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
+    accelerationChart.addTimeSeries(accelerationSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
+    accelerationChart.streamTo(document.getElementById("acceleration-chart"), 500);
 
-    smoothAx = new ExponentialMovingAverage(0.7);
-
-    var accellGravityChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
-    accellGravityChart.addTimeSeries(accellGravitySeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
-    accellGravityChart.addTimeSeries(accellGravitySeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
-    accellGravityChart.addTimeSeries(accellGravitySeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
-    accellGravityChart.streamTo(document.getElementById("accel-wg-chart"), 500);
+    var orientationChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
+    orientationChart.addTimeSeries(orientationSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
+    orientationChart.addTimeSeries(orientationSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
+    orientationChart.addTimeSeries(orientationSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
+    orientationChart.streamTo(document.getElementById("orientation-chart"), 500);
 
 }
 
@@ -67,7 +40,7 @@ function requestPermission() {
     DeviceMotionEvent.requestPermission()
       .then((state) => {
         if (state === 'granted') {
-          window.addEventListener('devicemotion', handleOrientation);
+          window.addEventListener('devicemotion', handleMotion);
         } else {
           console.error('Request to access the orientation was rejected');
         }
@@ -75,29 +48,24 @@ function requestPermission() {
       .catch(console.error);
   } else {
     // Handle regular non iOS 13+ devices.
-    window.addEventListener('devicemotion', handleOrientation);
+    window.addEventListener('devicemotion', handleMotion);
   }
 }
 
 
 
-function handleOrientation(event) {
+function handleMotion(event) {
 
   var now = Date.now();
-  gyroSeries1.append(now, event.rotationRate.alpha);
-  gyroSeries2.append(now, event.rotationRate.beta);
-  gyroSeries3.append(now, event.rotationRate.gamma);
+  rotationRateSeries1.append(now, event.rotationRate.alpha);
+  rotationRateSeries2.append(now, event.rotationRate.beta);
+  rotationRateSeries3.append(now, event.rotationRate.gamma);
 
-  smoothAx.update(event.acceleration.x);
+  accelerationSeries1.append(now, event.acceleration.x);
+  accelerationSeries2.append(now, event.acceleration.y);
+  accelerationSeries3.append(now, event.acceleration.z);
 
-  accellSeries1.append(now, event.acceleration.x);
-  //accellSeries2.append(now, smoothAx.filtered);
-  accellSeries2.append(now, event.acceleration.y);
-  accellSeries3.append(now, event.acceleration.z);
-
-  
-
-  accellGravitySeries1.append(now, event.accelerationIncludingGravity.x - event.acceleration.x);
-  accellGravitySeries2.append(now, event.accelerationIncludingGravity.y - event.acceleration.y);
-  accellGravitySeries3.append(now, event.accelerationIncludingGravity.z - event.acceleration.z);
+  orientationSeries1.append(now, event.accelerationIncludingGravity.x - event.acceleration.x);
+  orientationSeries2.append(now, event.accelerationIncludingGravity.y - event.acceleration.y);
+  orientationSeries3.append(now, event.accelerationIncludingGravity.z - event.acceleration.z);
 }
