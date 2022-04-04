9f130275b940028d057ead2496c72d3c2e3872e8
Ben Dyer <ben.dyer@hey.com>
2022-04-04 21:05:54 +0100

    Experiment with state detection

--- scripts/sensors.js
+++ scripts/sensors.js
@@ -1,4 +1,23 @@
 
+const AccelerationStates = {
+    Stationary: 'Stationary',
+    Accelerate: 'Accelerate',
+    Decelerate: 'Decelerate'
+  };
+
+const AccelerationThreshold = 0.5;
+ 
+const Direction = {
+  Undefined: 'Undefined',
+  Forwards: 'Forwards',
+  Backwards: 'Backwards',
+};
+
+var accelerationState = AccelerationStates.Stationary;
+var currentDirection = Direction.Undefined;
+
+
+// ---------------
 
 var rotationRateSeries1 = new TimeSeries();
 var rotationRateSeries2 = new TimeSeries();
@@ -14,23 +33,23 @@ var orientationSeries3 = new TimeSeries();
 
 function createTimeline() {
 
-    var rotationRateChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
-    rotationRateChart.addTimeSeries(rotationRateSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
-    rotationRateChart.addTimeSeries(rotationRateSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
-    rotationRateChart.addTimeSeries(rotationRateSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
-    rotationRateChart.streamTo(document.getElementById("rotation-rate-chart"), 500);
+  var rotationRateChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
+  rotationRateChart.addTimeSeries(rotationRateSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
+  rotationRateChart.addTimeSeries(rotationRateSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
+  rotationRateChart.addTimeSeries(rotationRateSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
+  rotationRateChart.streamTo(document.getElementById("rotation-rate-chart"), 500);
 
-    var accelerationChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
-    accelerationChart.addTimeSeries(accelerationSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
-    accelerationChart.addTimeSeries(accelerationSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
-    accelerationChart.addTimeSeries(accelerationSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
-    accelerationChart.streamTo(document.getElementById("acceleration-chart"), 500);
+  var accelerationChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
+  accelerationChart.addTimeSeries(accelerationSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
+  accelerationChart.addTimeSeries(accelerationSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
+  accelerationChart.addTimeSeries(accelerationSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
+  accelerationChart.streamTo(document.getElementById("acceleration-chart"), 500);
 
-    var orientationChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
-    orientationChart.addTimeSeries(orientationSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
-    orientationChart.addTimeSeries(orientationSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
-    orientationChart.addTimeSeries(orientationSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
-    orientationChart.streamTo(document.getElementById("orientation-chart"), 500);
+  var orientationChart = new SmoothieChart({ responsive: true, scrollBackwards: false });
+  orientationChart.addTimeSeries(orientationSeries1, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 3 });
+  orientationChart.addTimeSeries(orientationSeries2, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
+  orientationChart.addTimeSeries(orientationSeries3, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.2)', lineWidth: 3 });
+  orientationChart.streamTo(document.getElementById("orientation-chart"), 500);
 
 }
 
@@ -68,4 +87,38 @@ function handleMotion(event) {
   orientationSeries1.append(now, event.accelerationIncludingGravity.x - event.acceleration.x);
   orientationSeries2.append(now, event.accelerationIncludingGravity.y - event.acceleration.y);
   orientationSeries3.append(now, event.accelerationIncludingGravity.z - event.acceleration.z);
+
+  updateState(event);
+  
+}
+
+function updateState(event) {
+  
+  switch (accelerationState) {
+    case AccelerationStates.Stationary: 
+      if (Math.abs(event.acceleration.z > AccelerationThreshold)) {
+        currentDirection = event.acceleration.z < 0 ? Direction.Forwards : Direction.Backwards; 
+        accelerationState = AccelerationStates.Accelerate; 
+        console.log('STATIONARY -> ACCELERATE', currentDirection);
+      }
+    break;
+
+    case AccelerationStates.Accelerate: 
+
+      if (Math.abs(event.acceleration.z > AccelerationThreshold)) {
+
+        if(currentDirection == Direction.Forwards && event.acceleration.z < 0) {
+          accelerationState = AccelerationStates.Decelerate;
+          console.log('ACCELERATE -> DECELERATE');
+        }  
+      }
+    break;
+
+    case AccelerationStates.Decelerate: 
+      if (Math.abs(event.acceleration.z < AccelerationThreshold)) {
+        accelerationState = AccelerationStates.Stationary;
+        console.log('DECELERATE -> STATIONARY');
+      }
+      break;
+  }
 }
